e88c7c0e8070a0b803eb69bbfff11d18532bbada
Hamilton Evans <hamevans@Hamiltons-MacBook-Pro.local>
2021-09-05 18:20:27 -0700

    Footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './components/About'
 import Roadmap from './components/Roadmap'
 import Team from './components/Team'
 import Info from './components/Info'
+import Footer from './components/Footer'
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 
@@ -24,7 +25,8 @@ class App extends Component {
           <Roadmap />
           <Team />
           <Info />
-          </Router> 
+          <Footer />
+        </Router> 
       </>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'; 
+import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons'
+
+import "./Footer.css";
+
+export default class Footer extends Component { 
+  render () {
+    return (
+      <nav className="footer" id="#fullhome">
+        <div className="footer-container">
+          <HashLink smooth to="#fullhome" className="footer-logo">
+            GIRAFFES
+          </HashLink>
+
+          <ul className="footer-menu">
+            <li className="footer-item">
+              <Link className="footer-links" to={{ pathname: "https://twitter.com/BOIDS_NFT" }} target="_blank" >
+                <FontAwesomeIcon icon={faTwitter} />
+              </Link>
+            </li>
+            <li className="footer-item">
+              <Link className="footer-links" to={{ pathname: "https://discord.gg/XvWwuVdQ" }} target="_blank" >
+                <FontAwesomeIcon icon={faDiscord} />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
